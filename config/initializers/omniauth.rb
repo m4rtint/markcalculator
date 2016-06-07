df406c0f24d009e97e007987a1fc68440a32081e
Martin <m4rtin.t@gmail.com>
2016-06-07 13:03:32 -0700

    fixing google omni-auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, 415993921311-6pkh6n5cgeqnpol25nrgt9nqc8but26u.apps.googleusercontent.com, 4GGlkUpExVXJyB19EtHGePzX
 end
